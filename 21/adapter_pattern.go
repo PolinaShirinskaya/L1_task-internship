eadd4c1c9dcbbe27d08ffc3d7eb3592b39ca3b1c
adian <paulina.ad.sh@gmail.com>
2023-09-25 19:12:48 +0300

    add 21 task

--- 21/adapter_pattern.go
+++ 21/adapter_pattern.go
@@ -1,8 +1,61 @@
 // Реализовать паттерн «адаптер» на любом примере
 
-/* Паттерн адаптера состоит из 4 элементов:
-	1. Target - это интерфейс, который клиент хочет взаимодействовать
-	2. Adaptee - интерфейс, которую необходимо адаптировать для использования клиентом 
-	3. Adapter - адаптирует Adaptee к целевому интерфейсу 'Target
-	4. Client - управляет адаптером, косвенно вызывая метод адаптируемого объекта
-*/
+package main
+
+import "fmt"
+
+// Transport - интерфейс транспорта
+type Transport interface {
+	Drive()
+}
+
+// Auto - класс машины
+type Auto struct{}
+
+func (a Auto) Drive() {
+	fmt.Println("Машина едет по дороге")
+}
+
+// Driver - класс путешественника
+type Driver struct{}
+
+func (d Driver) Travel(transport Transport) {
+	transport.Drive()
+}
+
+// Animal - интерфейс животного
+type Animal interface {
+	Move()
+}
+
+// Camel - класс верблюда
+type Camel struct{}
+
+func (c Camel) Move() {
+	fmt.Println("Верблюд идет по пескам пустыни")
+}
+
+// CamelToTransportAdapter - адаптер от Camel к Transport
+type CamelToTransportAdapter struct {
+	camel Camel
+}
+
+func (a CamelToTransportAdapter) Drive() {
+	a.camel.Move()
+}
+
+func main() {
+	// путешественник
+	driver := Driver{}
+	// машина
+	auto := Auto{}
+	// отправляемся в путешествие
+	driver.Travel(auto)
+
+	// встретились пески, надо использовать верблюда
+	camel := Camel{}
+	// используем адаптер
+	camelTransport := CamelToTransportAdapter{camel}
+	// продолжаем путь по пескам пустыни
+	driver.Travel(camelTransport)
+}
